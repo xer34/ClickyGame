3ce5f3fa7b5f04ca97e9900f41a15b1092eadb20
Xer34 <experimental@gmail.com>
2019-01-17 19:05:15 -0500

    Updates

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Index from "./components/Body/Index";
+
+function App() {
+  return <Index />;
+}
+
+export default App;
---
+++ src/imageData.js
@@ -0,0 +1,64 @@
+const imageData = [
+    {
+        id: 1,
+        url: "https://picsum.photos/200/300?image=1",
+        clicked: false
+    },
+    {
+        id: 2,
+        url: "https://picsum.photos/200/300?image=20",
+        clicked: false
+    },
+    {
+        id: 3,
+        url: "https://picsum.photos/200/300?image=30",
+        clicked: false
+    },
+    {
+        id: 4,
+        url: "https://picsum.photos/200/300?image=40",
+        clicked: false
+    },
+    {
+        id: 5,
+        url: "https://picsum.photos/200/300?image=50",
+        clicked: false
+    },
+    {
+        id: 6,
+        url: "https://picsum.photos/200/300?image=60",
+        clicked: false
+    },
+    {
+        id: 7,
+        url: "https://picsum.photos/200/300?image=70",
+        clicked: false
+    },
+    {
+        id: 8,
+        url: "https://picsum.photos/200/300?image=80",
+        clicked: false
+    },
+    {
+        id: 9,
+        url: "https://picsum.photos/200/300?image=90",
+        clicked: false
+    },
+    {
+        id: 10,
+        url: "https://picsum.photos/200/300?image=100",
+        clicked: false
+    },
+    {
+        id: 11,
+        url: "https://picsum.photos/200/300?image=110",
+        clicked: false
+    },
+    {
+        id: 12,
+        url: "https://picsum.photos/200/300?image=120",
+        clicked: false
+    },
+]
+
+export default imageData
