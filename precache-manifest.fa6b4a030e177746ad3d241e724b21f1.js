f1d45782244a9120c49dce1c1a56af04de58bf4f
Xer34 <experimental@gmail.com>
2019-01-18 22:14:32 -0500

    Updates

---
+++ precache-manifest.fa6b4a030e177746ad3d241e724b21f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db23d507fdfb8226d095",
+    "url": "/Clickygame/static/js/runtime~main.db23d507.js"
+  },
+  {
+    "revision": "98d72f214cb5a92348f8",
+    "url": "/Clickygame/static/js/main.98d72f21.chunk.js"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "/Clickygame/static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "98d72f214cb5a92348f8",
+    "url": "/Clickygame/static/css/main.9a9a3b78.chunk.css"
+  },
+  {
+    "revision": "16fd21aee66329b82d58a9673b0c9f3e",
+    "url": "/Clickygame/index.html"
+  }
+];
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Index from "./components/Body/Index";
-
-function App() {
-  return <Index />;
-}
-
-export default App;
